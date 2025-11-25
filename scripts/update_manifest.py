ee07d8b3edb0beadab50922caadd540c77a3067c
rmawatson <rmawatson@gmail.com>
2025-11-25 05:14:17 +0100

    added includes to manifest

--- scripts/update_manifest.py
+++ scripts/update_manifest.py
@@ -19,27 +19,28 @@ def update_manifest():
     last_manifest_filename = manifest_filename
     last_manifest_schemas = {}
 
-    while True:
-        all_manifest_files.append(manifest_filename)
-        with open(manifest_path / manifest_filename, 'r', encoding='utf-8') as f:
-            manifest = json.load(f)
-        manifest_schemas = manifest.get("schemas", {})
+    if (manifest_path / manifest_filename).exists():
+        while True:
+            all_manifest_files.append(manifest_filename)
+            with open(manifest_path / manifest_filename, 'r', encoding='utf-8') as f:
+                manifest = json.load(f)
+            manifest_schemas = manifest.get("schemas", {})
 
-        all_mappings.update(manifest_schemas)
-        last_manifest_filename = manifest_filename
-        last_manifest_schemas = manifest_schemas
+            all_mappings.update(manifest_schemas)
+            last_manifest_filename = manifest_filename
+            last_manifest_schemas = manifest_schemas
 
-        if manifest.get("next") is None:
-            break
+            if manifest.get("next") is None:
+                break
 
-        next_manifest_file = manifest.get("next")
+            next_manifest_file = manifest.get("next")
 
-        if not (manifest_path / next_manifest_file).exists():
-            raise ManifestError(f"Manifest file '{next_manifest_file}' referenced_by '{manifest_filename}' not found")
-        
-        manifest_filename = next_manifest_file
-        index += 1
-
+            if not (manifest_path / next_manifest_file).exists():
+                raise ManifestError(f"Manifest file '{next_manifest_file}' referenced_by '{manifest_filename}' not found")
+            
+            manifest_filename = next_manifest_file
+            index += 1
+    
     new_manifest_entries = {}
 
     unverified_includes = defaultdict(list)
@@ -48,7 +49,7 @@ def update_manifest():
                         filename.name != "index.json" and \
                         filename.suffix == ".json"):
         file_mappings = []
-
+        file_includes = []
         if json_file.stem in all_mappings.keys():
             continue
         
@@ -69,12 +70,13 @@ def update_manifest():
                 if "include" in json_data["schemas"][schema_name]:
                     for include in json_data["schemas"][schema_name]["include"]:
                         unverified_includes[include].append(json_file.name)
-
+                        file_includes.append(include)
+                    
         if "mappings" in json_data:
             for mapping in json_data["mappings"]:
                 file_mappings.append(mapping)
         
-        new_manifest_entries[json_file.stem] = file_mappings
+        new_manifest_entries[json_file.stem] = {"mappings":file_mappings,"includes":file_includes}
 
     available_includes = list(all_mappings.keys()) + list(new_manifest_entries.keys())
 
@@ -104,7 +106,7 @@ def update_manifest():
         
         if remaining_entries:
             index += 1
-            new_manifest_filename = f"manifest.{index}.json"
+            new_manifest_filename = f"manifest.{index}.json" if index else "manifest.json"
             created_manifests.append(new_manifest_filename)
             with open(manifest_path / current_manifest_filename, 'r', encoding='utf-8') as f:
                 prev_manifest = json.load(f)
